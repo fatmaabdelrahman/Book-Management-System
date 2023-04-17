17f9f61261c1094b20fb94564951fe5e496a0f73
fatma.abdelrahman <fatma.abdelrahman@tafweela.com>
2023-04-17 06:52:11 +0200

    store note

--- controller/noteController.js
+++ controller/noteController.js
@@ -4,30 +4,41 @@ var memStorage = require('../util/memory.storage');
 var model = require('../model/note.model')
 
 exports.index = function(req,res){
-  var seqId   = generator.generate();
-    memStorage.store.setItem(seqId , "1st_key");
-    var seqId_2   = generator.generate();
-    memStorage.store.setItem(seqId_2 , "2nd_key");
+//   var seqId   = generator.generate();
+    // memStorage.store.setItem(seqId , "1st_key");
+    // var seqId_2   = generator.generate();
+    // memStorage.store.setItem(seqId_2 , "2nd_key");
 
-    var keys = memStorage.getKeys(memStorage.store);
+    // var keys = memStorage.getKeys(memStorage.store);
 
-    var keys = memStorage.getKeys(memStorage.store);
+    // var keys = memStorage.getKeys(memStorage.store);
+    // var values = memStorage.getValues(memStorage.store);
+
+    // var Note = model.Note;
+    // var notObj = new Note(seqId, "ccc" , " ssss" , "dddd", new Date());
+    // res.send("get all notes.....KEYS......"+ JSON.stringify(notObj));
+
     var values = memStorage.getValues(memStorage.store);
-
-    var Note = model.Note;
-    var notObj = new Note(seqId, "ccc" , " ssss" , "dddd", new Date());
-    res.send("get all notes.....KEYS......"+ JSON.stringify(notObj));
+    console.log("Values ....... " + JSON.stringify(values));
+    return res.status(200).send(JSON.stringify(values));
 
 }
 
 exports.store = (req,res)=>{
     var seqId   = generator.generate();
     // req.body 
-    var title = req.title;
-    var content = req.content;
-    var createdBy = req.createdBy;
+    var title = req.body.title;
+    var content = req.body.content;
+    var createdBy = "admin";
     var createdOn = new Date();
-    res.send("save new nte");
+    if(!title || !content){
+        return res.status(500).send({error: "Title or content don't have values"})
+    }
+
+    var Note = model.Note;
+    var notObj = new Note(seqId, title , content , createdBy, createdOn);
+    memStorage.store.setItem(seqId , notObj);
+   return  res.status(200).send("Note added successfully ! ");
 
 }
 
