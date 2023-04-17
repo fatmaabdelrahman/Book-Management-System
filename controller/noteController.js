e76078a4a893dd35dedb8950b7ef2913c9595405
fatma.abdelrahman <fatma.abdelrahman@tafweela.com>
2023-04-17 04:32:21 +0200

    fetch all notes from memory storage

--- controller/noteController.js
+++ controller/noteController.js
@@ -1,11 +1,32 @@
+const { json } = require('body-parser');
 var generator =require('../util/generator');
+var memStorage = require('../util/memory.storage');
+var model = require('../model/note.model')
 
 exports.index = function(req,res){
-    var seqIds   = generator.generate(); 
-    res.send("get all notes"+ seqIds);
+  var seqId   = generator.generate();
+    memStorage.store.setItem(seqId , "1st_key");
+    var seqId_2   = generator.generate();
+    memStorage.store.setItem(seqId_2 , "2nd_key");
+
+    var keys = memStorage.getKeys(memStorage.store);
+
+    var keys = memStorage.getKeys(memStorage.store);
+    var values = memStorage.getValues(memStorage.store);
+
+    var Note = model.Note;
+    var notObj = new Note(seqId, "ccc" , " ssss" , "dddd", new Date());
+    res.send("get all notes.....KEYS......"+ JSON.stringify(notObj));
+
 }
 
 exports.store = (req,res)=>{
+    var seqId   = generator.generate();
+    // req.body 
+    var title = req.title;
+    var content = req.content;
+    var createdBy = req.createdBy;
+    var createdOn = new Date();
     res.send("save new nte");
 
 }
