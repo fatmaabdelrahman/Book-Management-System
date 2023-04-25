2ac7433dc3ea86d5298b7ee7dfa32041b1f3e4ed
fatmaabdelrahman <fabdelrahman72@yahoo.com>
2023-04-25 05:38:36 +0200

    add Db schema

---
+++ scripts/ddl.sql
@@ -0,0 +1,28 @@
+CREATE TABLE book_store.books (
+    id   serial NOT NULL,  
+    title   varchar (250) NOT NULL,
+    isbn varchar (50) NOT NULL,
+    description varchar (1000)  null,
+    publisher varchar (50) NOT null,
+    pages varchar (5)  null,
+    store_code varchar (5) NOT null,
+    created_on timestamp NOT null,
+    created_by varchar (50) NOT null,
+    constraint books_pkey primary key (id) 
+);
+
+-- books.store definition
+
+-- Drop table
+
+-- DROP TABLE books.store;
+
+CREATE TABLE book_store.stores (
+	id serial NOT NULL,
+	store_name varchar(100) NOT NULL,
+	store_code varchar(5) NOT NULL,
+	created_on timestamp NOT NULL,
+	created_by varchar(50) NOT NULL,
+	address varchar(200) NOT NULL,
+	CONSTRAINT store_pkey PRIMARY KEY (id)
+);
--- server.js
+++ server.js
@@ -3,16 +3,19 @@ const express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser')
 const app = express();
-const noteRouter= require("./route/noteRoute");
+var storeRoute = require('./route/store.route');
+var bookRoute = require('./route/book.route');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
-
 app.use(cors());
-app.use("/api/v1",noteRouter);
+
+
+app.use("/api/v1" , storeRoute);
+app.use("/api/v1" , bookRoute);
 
 app.get("/",function(req,res){
     res.send("server Started ......")
